cd14eff09f34dd92550ca6189579c296f16442b7
Max Brown <mb39@sanger.ac.uk>
2022-03-09 15:18:24 +0000

    Try again...

--- make_docs.bash
+++ make_docs.bash
@@ -5,6 +5,6 @@ cargo doc --no-deps
 # remove old docs
 rm -rf ./docs
 # magic..?
-echo "<meta http-equiv=\"refresh\" content=\"0; url=build_wheel\">" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=gfatk\">" > target/doc/index.html
 # copy to docs
 cp -r target/doc ./docs
