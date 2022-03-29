9bf0f91cb9e6588925342bc24dac0583eefbb2eb
Max Brown <mb39@sanger.ac.uk>
2022-03-29 15:55:33 +0100

    Arrow to comma in fasta header.

--- src/gfa/graph.rs
+++ src/gfa/graph.rs
@@ -408,7 +408,7 @@ impl GFAdigraph {
 
             // no spaces between the formatted strings
             if index == 0 {
-                chosen_path_string.push(format!("{}{}->{}{}", from, from_orient, to, to_orient));
+                chosen_path_string.push(format!("{}{},{}{}", from, from_orient, to, to_orient));
             } else {
                 chosen_path_string.push(format!("->{}{}", to, to_orient));
             }
